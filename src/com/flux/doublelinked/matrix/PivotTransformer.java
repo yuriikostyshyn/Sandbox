e654a6c3cdfc52703848f214c17557bf40b7cba2
yurii_kostyshyn <yuriywert@gmail.com>
2013-09-04 09:13:38 +0300

    work on sparsed matrix

--- src/com/flux/doublelinked/matrix/PivotTransformer.java
+++ src/com/flux/doublelinked/matrix/PivotTransformer.java
@@ -9,14 +9,48 @@ import com.flux.doublelinked.matrix.SparsedMatrix.Node;
 public class PivotTransformer {
 	public static SparsedMatrix transformMatrix(SparsedMatrix matrix, int rowId, int columnId) {
 		SparsedMatrix result = new SparsedMatrix(matrix.getWidth(), matrix.getHeight());
-		//TODO copy matrix
-		Node p0 = getRowHeader(rowId, matrix);
-		Node q0 = getColumnHeader(columnId, matrix);
+		// TODO copy matrix
+		Node pivotRowHeader = getRowHeader(rowId, matrix);
+		Node pivotColumnHeader = getColumnHeader(columnId, matrix);
 		Node[] ptr = new Node[matrix.getWidth()];
+		Node[] columnHeaders = new Node[matrix.getWidth()];
+		Node[] rowHeaders = new Node[matrix.getHeight()];
+		initColumnHeadersReferences(columnHeaders, matrix.getHeader());
+		initRowHeadersReferences(rowHeaders, matrix.getHeader());
 
 		BigDecimal pivotValue = matrix.getValue(rowId, columnId);
 
-		init(pivotValue, p0, ptr, matrix.getHeader());
+		init(pivotValue, pivotRowHeader, ptr, matrix.getHeader());
+
+		pivotColumnHeader = pivotColumnHeader.getTop();
+		while (pivotColumnHeader.getColumn() >= 0) {
+			int i = pivotColumnHeader.getRow();
+			if (i != rowId) {
+				Node p = rowHeaders[i];
+				Node p1 = p.getLeft();
+				pivotRowHeader = pivotRowHeader.getLeft();
+				while (pivotRowHeader.getColumn() > 0) {
+					int j = pivotRowHeader.getColumn();
+					while (p1.getColumn() > j) {
+						p = p1;
+						p1 = p.getLeft();
+					}
+					if (p1.getColumn() == j) {
+						// TODO pivotal transformation
+					} else {
+						// TODO insert new element
+					}
+
+					if (j == columnId) {
+						break;
+					} else if (j < 0) {
+						pivotColumnHeader.setValue(pivotColumnHeader.getValue().divide(pivotValue.negate()));
+					}
+					pivotRowHeader = pivotRowHeader.getLeft();
+				}
+			}
+			pivotColumnHeader = pivotColumnHeader.getTop();
+		}
 
 		return result;
 	}
@@ -49,26 +83,42 @@ public class PivotTransformer {
 		}
 	}
 
+	private static void initColumnHeadersReferences(Node[] columnHeaders, Node header) {
+		Node current = header.getLeft();
+		while (current.getColumn() >= 0) {
+			columnHeaders[current.getColumn()] = current;
+			current = current.getLeft();
+		}
+	}
+
+	private static void initRowHeadersReferences(Node[] rowHeaders, Node header) {
+		Node current = header.getTop();
+		while (current.getRow() >= 0) {
+			rowHeaders[current.getRow()] = current;
+			current = current.getTop();
+		}
+	}
+
 	private static void init(BigDecimal pivotValue, Node rowHeader, Node[] ptr, Node header) {
-		Node current = rowHeader.getLeft();
+		rowHeader = rowHeader.getLeft();
 		Node currentHeaderElement = header.getLeft();
-		while (current.getColumn() > 0) {
-			setModifiedPivotRowElementValue(pivotValue, current);
-			currentHeaderElement = initReferenceToColumnHeader(ptr, current, currentHeaderElement);
+		while (rowHeader.getColumn() >= 0) {
+			setModifiedPivotRowElementValue(pivotValue, rowHeader);
+			currentHeaderElement = initReferenceToColumnHeader(ptr, rowHeader, currentHeaderElement);
 		}
 	}
 
 	private static Node initReferenceToColumnHeader(Node[] ptr, Node current, Node currentHeaderElement) {
-	    while (currentHeaderElement.getColumn() > current.getColumn()) {
-	    	currentHeaderElement = currentHeaderElement.getLeft(); // hmm. Maybe we don't need next condition statement
-	    }
-	    if (currentHeaderElement.getColumn() == current.getColumn()) {
-	    	ptr[currentHeaderElement.getColumn()] = currentHeaderElement;
-	    }
-	    return currentHeaderElement;
-    }
+		while (currentHeaderElement.getColumn() > current.getColumn()) {
+			currentHeaderElement = currentHeaderElement.getLeft(); // hmm. Maybe we don't need next condition statement
+		}
+		if (currentHeaderElement.getColumn() == current.getColumn()) {
+			ptr[currentHeaderElement.getColumn()] = currentHeaderElement;
+		}
+		return currentHeaderElement;
+	}
 
 	private static void setModifiedPivotRowElementValue(BigDecimal pivotValue, Node current) {
-	    current.setValue(current.getValue().divide(pivotValue, new MathContext(5)));
-    }
+		current.setValue(current.getValue().divide(pivotValue, new MathContext(5)));
+	}
 }
